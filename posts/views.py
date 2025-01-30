ec6ce5a252a5c4dadeb57c6cb310eb7f3482c8ce
daryle <daryle.tan@gmail.com>
2025-01-29 21:33:20 -0600

    created CRUD for Posts

--- posts/views.py
+++ posts/views.py
@@ -20,4 +20,34 @@ class PostView(APIView):
             serializer = PostSerializer(posts, many=True)
             return Response(serializer.data)
     
+    def post(self, request):
+        serializer = PostSerializer(data=request.data)
+        
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
+    def put(self, request, post_id):
+        posts = Posts.objects.get(id=post_id)
+
+        if "post_title" in request.data:
+            posts.post_title_update(request.data["post_title"])
+            posts.save()
+        if "post_content" in request.data:
+            posts.post_content_update(request.data["post_content"])
+            posts.save()
+        if "movie_genre" in request.data:
+            posts.movie_genre_update(request.data["movie_genre"])
+            posts.save()
+        
+        serializer = PostSerializer(posts)
+        return Response(serializer.data)
+    
+    def delete(self, request, category_id):
+        try:
+            posts = Posts.objects.get(id=category_id)
+            posts.delete()
+            return Response({"success": "Post deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
+        except Posts.DoesNotExist:
+            return Response({"error": "Post not found"}, status=status.HTTP_404_NOT_FOUND)
